3b735c39c0d175bfaa4e3239565ca2389144db09
kamal al khadhrawi <kmkart2000@hotmail.com>
2021-03-01 03:57:02 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
 //Form
-var button= document.querySelector(''); 
-var input= document.querySelector(''); 
+//var button= document.querySelector('.icon'); 
+
+var input= document.querySelector('#city-input'); 
 //City & weather
-var city= document.querySelector(''); 
-var weather= document.querySelector('');
+var city= document.querySelector('#city'); 
+var weather= document.querySelector('#weather');
 //Temperature
-var currentTemp= document.querySelector('');
-var minTemp= document.querySelector('');
-var maxTemp= document.querySelector('');
+var currentTemp= document.querySelector('#current');
+var minTemp= document.querySelector('#min');
+var maxTemp= document.querySelector('#max');
 //Info
-var wind= document.querySelector('');
-var humidity= document.querySelector('');
-var pressure= document.querySelector('');
-var icon= document.querySelector('');
+var wind= document.querySelector('#wind');
+var humidity= document.querySelector('#humidity');
+var pressure= document.querySelector('#pressure');
+var icon= document.querySelector('#weather-icon');
 
 //Open weather API key: 
-const apiKey= 'YOUR API key here'; 
+const apiKey= '0593ba127263866b8b726854391b1be4'; 
 
 
-//Button Event listener 'click' 
+
+
+
+
+function Getweather (){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=${apiKey}`)
+  .then(function(response){ return response.json()})
+  .then(function(data){
+  console.log(data)
+
+    const city = data.name; 
+    const weather= data.weather[0].description;
+    const temp= Math.floor(data.main.temp);
+    const minTemp= Math.floor(data.main.temp_min);
+    const maxTemp= Math.floor(data.main.temp_max);
+
+    const wind=data.wind.speed;  
+    const pressure=Math.floor(data.main.pressure) ; 
+    const humidity= Math.floor(data.main.humidity); ; 
+    const icon= data.weather[0].icon;
+    const iconSrc=`http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+
+    //place the value
+    window.city.innerHTML= city;
+    window.weather.innerHTML= weather; 
+    window.currentTemp.innerHTML= `${temp}<sup>°</sup>`; 
+    window.minTemp.innerHTML= `${minTemp}<sup>°</sup>`;
+    window.maxTemp.innerHTML= `${maxTemp}<sup>°</sup>`;
+
+    window.wind.innerHTML=`Wind ${wind} m/s` ;
+    window.humidity.innerHTML=`Humidity ${humidity} %` ;
+    window.pressure.innerHTML=`pressure ${pressure} hPas` ; 
+    window.icon.src= iconSrc;
+   
+  })
+}
+    
+Getweather ();
